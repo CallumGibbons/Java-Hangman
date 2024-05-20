73c30d65bbcc4ab034b95e9eb4dfc19d6369a340
callum <callumgibbons@icloud.com>
2024-05-20 20:44:18 +0530

    Finished game logic, it can check whether a guessed letter is present in the word to guess and replaces the corresponding _s with the guessed letter. Users can also guess an entire word and the game will check if it matches. Finished necessary getters and setters as well.

--- src/GameState.java
+++ src/GameState.java
@@ -16,4 +16,44 @@ public class GameState {
         this.guessedLetters = new ArrayList<>();
         this.triesLeft = 6;
     };
-}
+
+    public boolean isGameOver() {
+        return triesLeft <= 0 || isWordGuessed();
+    }
+    public boolean isWordGuessed() {
+        return !guessedWord.contains('_');
+    }
+    public boolean isLetterGuessed(char letter) {
+        return guessedLetters.contains(letter);
+    }
+    public void guessLetter(char letter) {
+        guessedLetters.add(letter);
+
+        if (wordToGuess.indexOf(letter) >= 0) {
+            for (int i = 0; i < wordToGuess.length(); i++) {
+                if (wordToGuess.charAt(i) == letter) {
+                    guessedWord.set(i, letter);
+                }
+            }
+            System.out.println("Correct guess!");
+        } else {
+            triesLeft--;
+            System.out.println("Wrong guess.");
+        }
+    }
+    public String getWordToGuess() {
+        return wordToGuess;
+    }
+
+    public String getGuessedWord() {
+        StringBuilder result = new StringBuilder();
+        for (Character c : guessedWord) {
+            result.append(c);
+        }
+        return result.toString();
+    }
+
+    public int getTriesLeft() {
+        return triesLeft;
+    }
+};
