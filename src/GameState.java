cbaabb99c0f883fa272c88b92f3f3c0b85187b21
callum <callumgibbons@icloud.com>
2024-05-20 19:21:06 +0530

    Created initial variable to be used by the gamestate as well as the necessary constructor for them. This is the logic that adds the necessary _ to replace letters and sets the number of lives left.

--- src/GameState.java
+++ src/GameState.java
@@ -1,2 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameState {
+    private String wordToGuess;
+    private List<Character> guessedWord;
+    private List<Character> guessedLetters;
+    private int triesLeft;
+
+    public GameState(String wordToGuess) {
+        this.wordToGuess = wordToGuess;
+        this.guessedWord = new ArrayList<>();
+        for (int i = 0; i < wordToGuess.length(); i++) {
+            guessedWord.add('_');
+        }
+        this.guessedLetters = new ArrayList<>();
+        this.triesLeft = 6;
+    };
 }
